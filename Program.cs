320776915a7b243bdd97d4df00df72bdeb01aa30
Zurab Shakulashvili <zshakulashvili@europebet.com>
2023-06-05 16:41:03 +0400

    done(imedia)

---
+++ Program.cs
@@ -0,0 +1,179 @@
+﻿
+
+using JsonSerialize;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+
+public static class JsonSerializer
+{
+
+    private static string getString(object val)
+    {
+
+        if(val == null)
+        {
+            return "null";
+        }
+        if(val is string)
+        {
+            return val.ToString();
+        }
+        if (val is bool)
+        {
+            return ((bool)val) ? "true" : "false";
+        }
+        if (val is int || val is double || val is float || val is decimal)
+        {
+            return val.ToString();
+        }
+        if(val is IList)
+        {
+            IList list = (IList)val;
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+            foreach(object o in list)
+            {
+                if(o is string || o.GetType().IsPrimitive)
+                {
+                    sb.Append(getString(o));
+                    sb.Append(',');
+                }
+                else
+                {
+                    MethodInfo m = typeof(JsonSerializer).GetMethod("Serialize").MakeGenericMethod(o.GetType());
+                    sb.Append((string)m.Invoke(null, new object[] {o}));
+                    sb.Append(',');
+                }
+            }
+            sb.Remove(sb.Length - 1, 1);
+            sb.Append(']');
+            return sb.ToString();
+        }
+        MethodInfo rec = typeof(JsonSerializer).GetMethod("Serialize").MakeGenericMethod(val.GetType());
+
+        return (string)rec.Invoke(null, new object[] {val});
+
+    }
+    public static string Serialize<T>(T data)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("{");
+        Type t = typeof(T);
+        foreach (PropertyInfo pi in t.GetProperties())
+        {
+            var propValue = pi.GetValue(data);
+            sb.Append("\"" + pi.Name + "\":" + getString(propValue));
+            sb.Append(",");
+        }
+        sb.Remove(sb.Length - 1, 1);
+        sb.Append("}");
+        return sb.ToString();
+    }
+
+
+    public static T DeserializeRec<T>(Dictionary<string, object> JsonKV)
+    {
+        Type type = typeof(T);
+        var properties = type.GetProperties();
+        T res = (T)FormatterServices.GetUninitializedObject(type);
+        foreach (var prop in properties)
+        {
+            if (prop.PropertyType.IsPrimitive || prop.PropertyType == typeof(string))
+            {
+                string k = $"\"{prop.Name}\"";
+                prop.SetValue(res, Convert.ChangeType(JsonKV[k], prop.PropertyType));
+                continue;
+            }
+            if (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(List<>))
+            {
+                string k = $"\"{prop.Name}\"";
+
+                Type t = prop.PropertyType.GetGenericArguments()[0];
+
+                Type listType = typeof(List<>).MakeGenericType(t);
+                IList ListToSet = (IList)Activator.CreateInstance(listType);
+                List<object> ListToSetFrom = (List<object>)JsonKV[k];
+                foreach(object o in ListToSetFrom)
+                {
+                    if (t.IsPrimitive || t == typeof(string))
+                    {
+                        ListToSet.Add(Convert.ChangeType(o, t));
+                    }
+                    else
+                    {
+                        MethodInfo recMet = typeof(JsonSerializer).GetMethod("DeserializeRec").MakeGenericMethod(t);
+                        ListToSet.Add(recMet.Invoke(null, new object[] { o }));
+                    }
+                    prop.SetValue(res, ListToSet);
+                }
+            }
+            else
+            {
+                MethodInfo recMet = typeof(JsonSerializer).GetMethod("DeserializeRec").MakeGenericMethod(prop.PropertyType);
+                string k = $"\"{prop.Name}\"";
+                prop.SetValue(res, recMet.Invoke(null, new object[] { JsonKV[k] }));
+
+            }
+        }
+        return res;
+    }
+    
+
+    public static T Deserialize<T>(string json)
+    {
+        Dictionary<string, object> JsonKV = jsonParser.Parse(json);
+        Type type = typeof(T);
+        MethodInfo recMet = typeof(JsonSerializer).GetMethod("DeserializeRec").MakeGenericMethod(type);
+        T res = (T)recMet.Invoke(null, new object[] {JsonKV});
+
+
+        return res;
+    }
+
+    public static void Main()
+    {
+        string json = File.ReadAllText(@"C:\Users\zshakulashvili\source\repos\BankAccount\JsonSerialize\jsonExample.json");
+        example e = Deserialize<example>(json);
+        Console.WriteLine(e.name);
+        menuClass m = e.menu;
+        Console.WriteLine(m.food1);
+        Console.WriteLine(m.food2);
+        List<int> l = e.list;
+        foreach (int i in l)
+        {
+            Console.WriteLine(i);
+        }
+
+        List<objExample> lo = e.listOfObjects;
+        foreach (objExample objExample in lo)
+        {
+            Console.WriteLine(objExample.objName);
+        }
+
+        Console.WriteLine(Serialize<example>(e));
+    }
+
+    public class example
+    {
+        public menuClass menu { get; set; }
+        public string name { get; set; }
+        public List<int> list { get; set; }
+        public List<objExample> listOfObjects { get; set; }
+    }
+
+    public class menuClass
+    {
+        public int food1 { get; set; }
+        public int food2 { get; set;}
+
+    }
+
+    public class objExample
+    {
+        public string objName { get; set; }
+    }
+}
---
+++ jsonParser.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonSerialize
+{
+
+
+    static class jsonParser
+    {
+        private static readonly HashSet<char> _chars = new HashSet<char>{'\n', '\t',
+                                                            '\r', '\b', '\f', '\a', '\0', '\v'};
+
+        private static bool IsEscapeChar(char c)
+        {
+            return _chars.Contains(c);
+        }
+
+        private static Dictionary<int,int> GetBracketPairs(StringBuilder sb)
+        {
+            Dictionary<int, int> res = new Dictionary<int, int>();
+            Stack<int> stack = new Stack<int>();
+
+            for(int i = 0; i < sb.Length; i++)
+            {
+                char c = sb[i];
+                if (sb[i] == '{' || sb[i] == '[')
+                {
+                    stack.Push(i);
+                    continue;
+                }
+                if (sb[i] == '}' || sb[i] == ']')
+                {
+                    int ind = stack.Pop();
+                    res[ind] = i;
+                }
+            }
+
+            return res;
+        }
+
+        public static Dictionary<string, string> JsonToDict(StringBuilder sb,
+            Dictionary<int, int> bracketPairs, int left, int right)
+        {
+
+            Dictionary<string, string> res = new Dictionary<string, string>();
+            StringBuilder curString = new StringBuilder();
+            Stack<StringBuilder> st = new Stack<StringBuilder>();
+
+            for(int i = left; i < right; i++)
+            {
+
+                if (sb[i] == ',')
+                {
+                    st.Push(curString);
+                    string val = st.Pop().ToString();
+                    string key = st.Pop().ToString();
+                    res[key] = val;
+                    curString = new StringBuilder();
+                    continue;
+                }
+                
+                if (sb[i] == ':')
+                {
+                    st.Push(curString);
+                    curString = new StringBuilder();
+                    continue;
+                }
+                if (sb[i] == '{' || sb[i] == '[')
+                {
+                    curString = (new StringBuilder(sb.ToString().Substring(i, bracketPairs[i] - i + 1)));
+                    i = bracketPairs[i];
+                    continue;
+                }
+                curString.Append(sb[i]);
+            }
+
+            st.Push(curString);
+            while(st.Count > 0)
+            {
+                string val = st.Pop().ToString();
+                 string key = st.Pop().ToString();
+                res[key] = val;
+            };
+            foreach(KeyValuePair<string,string> kv in res)
+            {
+                Console.WriteLine($"Key: {kv.Key}, Value: {kv.Value}");
+            }
+
+            return res;
+        }
+
+        public static Dictionary<string, object> toDict(StringBuilder sb, Dictionary<int, int> bracketPairs
+            , int left, int right){
+
+            Dictionary<string, object> res = new Dictionary<string, object>();
+            StringBuilder curString = new StringBuilder();
+            Stack<StringBuilder> st = new Stack<StringBuilder>();
+
+            for (int i = left; i < right; i++)
+            {
+
+                if (sb[i] == ',')
+                {
+                    st.Push(curString);
+                    string val = st.Pop().ToString();
+                    string key = st.Pop().ToString();
+                    res[key] = val;
+                    curString = new StringBuilder();
+                    continue;
+                }
+
+                if (sb[i] == ':')
+                {
+                    st.Push(curString);
+                    curString = new StringBuilder();
+                    continue;
+                }
+                if (sb[i] == '{')
+                {
+                    string key = st.Pop().ToString();
+                    res[key] = toDict(sb, bracketPairs, i + 1, bracketPairs[i]);
+                    i = bracketPairs[i] + 1;
+                    continue;
+                }
+                if (sb[i] == '[')
+                {
+                    List<object> list = new List<object>();
+                    StringBuilder t = new StringBuilder();
+                    for(int j = i + 1; j < bracketPairs[i]; j++)
+                    {
+                        if(sb[j] == '{')
+                        {
+                            object temp = toDict(sb, bracketPairs, j + 1, bracketPairs[j]);
+                            list.Add(temp);
+                            j = bracketPairs[j] + 1;
+                            t = new StringBuilder();
+                            continue;
+                        }
+                        if(sb[j] == ',')
+                        {
+                            list.Add(t.ToString());
+                            t = new StringBuilder();
+                            continue;
+                        }
+                        t.Append(sb[j]);
+                    }
+                    if (t.Length > 0) list.Add(t.ToString());
+                    i = bracketPairs[i] + 1;
+                    string key = st.Pop().ToString();
+                    res[key] = list;
+                    continue;
+                }
+                curString.Append(sb[i]);
+            }
+
+            if(st.Count != 0)
+                st.Push(curString);
+            while (st.Count > 0)
+            {
+                string val = st.Pop().ToString();
+                string key = st.Pop().ToString();
+                res[key] = val;
+            };
+            //foreach (KeyValuePair<string, string> kv in res)
+            //{
+            //    Console.WriteLine($"Key: {kv.Key}, Value: {kv.Value}");
+            //}
+
+            return res;
+        }
+
+
+        private static void printDict(Dictionary<string, object> dict)
+        {
+
+            foreach(KeyValuePair<string, object> kvp in dict)
+            {
+                object ob = kvp.Value;
+                if(ob is string)
+                {
+                    Console.WriteLine(kvp.Key + " : " + (string)ob);
+                    continue;
+                }
+                if(ob is List<object>)
+                {
+                    Console.WriteLine(kvp.Key + " : " + "list");
+                    List<object> r = (List<object>)ob;
+                    
+                    Console.WriteLine("size : " + r.Count);
+                    //foreach (object o in (List<object>)ob)
+                    //{
+                    //    printDict((Dictionary<string, object>)o);
+                    //}
+                    foreach (object o in r)
+                    {
+                        Console.WriteLine(o.ToString());
+                    }
+                    continue;
+                }
+                if(ob is Dictionary<string,object>)
+                {
+                    Console.WriteLine(kvp.Key);
+                    printDict((Dictionary<string, object>)ob);
+                }
+            }
+        }
+        public static Dictionary<string, object> Parse(string json)
+        {
+            //List<KeyValuePair<string, string>> res;
+            StringBuilder sb = new StringBuilder();
+            bool BracketIsOpen = false;
+
+            for(int i = 0; i < json.Length; i++)
+            {
+                char c = json[i];
+                if (IsEscapeChar(c)) continue;
+                if (c == ' ' && !BracketIsOpen) continue;
+                if (c == '"')
+                {
+                    sb.Append('"');
+                    BracketIsOpen = BracketIsOpen ? false : true;
+                    continue;
+                }
+                sb.Append(c);
+            }
+
+            Console.WriteLine(sb) ;
+            Dictionary<int, int> dict = GetBracketPairs(sb);
+            Dictionary<string, object> keyValuePairs = toDict(sb, dict, 1, sb.Length - 1);
+            //printDict(keyValuePairs);
+            //Dictionary<string, string> keyValuePairsk = JsonToDict(sb, dict, 1, sb.Length - 1);
+            return keyValuePairs;
+        }
+
+    }
+}
